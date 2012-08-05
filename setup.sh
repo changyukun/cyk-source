b25304b9abdf14c1e519bf1aa8bf8296be2a5c00
changyukun <changyukun@changyukun-desktop.(none)>
2012-08-05 15:21:04 +0800

    modify

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 ##################################################################
 #####	File Name   : 
 #####	Description :  
-#####	    Author   :  changyukun
+#####	    Author   :  changyuku n
 ##################################################################
 
 export COMPILE_DIR=/opt/STM/STLinux-2.3/devkit/sh4/bin/
